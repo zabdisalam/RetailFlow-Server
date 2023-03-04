bb40c2772f60dd140a45ea120fa9a288ae04f87f
Mohamed Dahir <59775077+Mdcoder4536@users.noreply.github.com>
2023-03-04 18:57:27 -0500

    Combined User methods

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import Task from '../models/Task.js'
 
 export const register = async(req, res) => {
   try {
@@ -15,5 +16,19 @@ export const register = async(req, res) => {
     console.log(user._doc);
   } catch (err) {
     throw err;
+  }
+}
+
+export const getUserTickets = async(req, res, next)=> {
+    const userId = req.params.userId;
+  try {
+    // Query the database to retrieve tickets associated with the user
+    const tickets = await Task.find({ assignedTo: userId }).exec();
+    // Return the tickets as a JSON response
+    res.json(tickets);
+  } catch (error) {
+    // Return an error response if the query fails
+    res.status(500).json({ message: error.message="UserId not found" });
+
   }
 }
--- routers/user.js
+++ routers/user.js
@@ -3,8 +3,14 @@ import {
   register,
 } from "../controllers/user.js";
 
+import {
+  getUserTickets,
+} from "../controllers/user.js";
+
 const router = express.Router();
 
 router.post("/", register);
+// The endpoint to retrieve tickets associated with a user
+router.get('/:userId/tickets', getUserTickets);
 
 export default router;
