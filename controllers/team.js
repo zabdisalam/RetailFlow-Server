194ea6fe6965e94a2607ea19d4c08ab89aa7a0e7
Ismail <daheeye1011@gmail.com>
2023-03-05 02:12:34 -0500

    get tasks under team http://localhost:8000/api/team/64042ef7b8b7eea9b2deb135

--- controllers/team.js
+++ controllers/team.js
@@ -1,14 +1,30 @@
-import Team from '../models/Team.js'
+import Team from "../models/Team.js";
+import Task from "../models/Task.js";
+import mongoose from "mongoose";
 
-export const newTeam = async(req, res)=>{
-    try {
-        const teamNew = new Team({
-            name: req.body.name
-        });
-        const team = await teamNew.save()
-        res.status(200).json(team._doc);
-        console.log(team._doc);
-    } catch(err){
-        throw err;
-    }
-}
+export const newTeam = async (req, res) => {
+  try {
+    const teamNew = new Team({
+      name: req.body.name,
+    });
+    const team = await teamNew.save();
+    res.status(200).json(team._doc);
+    console.log(team._doc);
+  } catch (err) {
+    throw err;
+  }
+};
+
+//Retrieves all Tasks assigned to a Team
+export const getTeamTasks = async (req, res, next) => {
+  const teamId = req.params.teamId;
+  try {
+    const tasks = await Task.find({
+      teamId: mongoose.Types.ObjectId(teamId),
+    }).populate("assignedTo reportedBy");
+    console.log("Get Tasks Under Team", tasks);
+    return res.status(200).json(tasks);
+  } catch (err) {
+    return res.status(500).json({ message: err.message="TeamID not found" });
+  }
+};
