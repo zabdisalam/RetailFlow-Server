12a1bde5230557fc1bf5a8064d340fb849940c59
Hanad5670 <hanad5670@gmail.com>
2023-03-04 19:06:28 -0500

    merged two user.js files that were in the router

--- routers/user.js
+++ routers/user.js
@@ -1,10 +1,15 @@
+import { getUserTickets } from '../controllers/user.js';
 import express from "express";
 import {
   register,
-} from "../controllers/user.js";
+} from "../controllers/auth.js";
 
 const router = express.Router();
 
+// The endpoint to retrieve tickets associated with a user
+router.get('/:userId/tickets', getUserTickets);
+
+// The endpoint to add tickets associated with a user
 router.post("/", register);
 
 export default router;
